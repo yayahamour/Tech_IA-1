c7d2ac69d1a8518628490cefde1e9e755b352b01
CharlySimplon <beaussart@protonmail.com>
2021-11-09 14:27:34 +0100

    Premier

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+
+
+def hello(my_first_name):
+    if my_first_name == "Charly":
+        return print("Hello {}!".format(my_first_name))
+    else:
+        return print("Je ne vous connais pas.")
+
+hello("Charly")
+hello("Marc")
