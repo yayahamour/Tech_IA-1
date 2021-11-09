2e60efb05d694bd89107a3c010a8d05e80e35a77
Yanis <yayahamour@gmail.com>
2021-11-09 15:02:08 +0100

    changement

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 
 
 def hello(my_first_name):
-    if my_first_name == "Charly":
+    if my_first_name == "yaya":
         return print("Hello {}!".format(my_first_name))
     else:
         return print("Je ne vous connais pas.")
